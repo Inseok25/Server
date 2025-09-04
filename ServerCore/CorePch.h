20e8a73a62ae79f3f215066107dfcfe67bc1af84
Inseok <Inseok25@gmail.com>
2025-09-04 22:37:21 +0900

    STL Allocator 구현

--- ServerCore/CorePch.h
+++ ServerCore/CorePch.h
@@ -1,22 +1,14 @@
 #pragma once
 
+#include <windows.h>
 #include <iostream>
+
 #include "Types.h"
 #include "CoreMacro.h"
 #include "CoreTLS.h"
 #include "CoreGlobal.h"
+#include "Container.h"
 
-#include <vector>
-#include <list>
-#include <queue>
-#include <stack>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-
-#include <windows.h>
-#include <iostream>
 using namespace std;
 
 #include "Lock.h"
